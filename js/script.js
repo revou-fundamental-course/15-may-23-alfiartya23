bf5b6c17965c2e14fb0bcc3c6938b44f5ff9f7c4
alfiartya23 <alfiartya.diwanda@gmail.com>
2023-05-24 12:41:49 +0700

    Implement script for select option logic, handling button luas persegi and reset

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
 "use strict";
+
+// Take the html tag
+const inputNumberLuas = document.querySelector("#input-sisi-luas");
+const inputNumberKeliling = document.querySelector("#input-sisi-keliling");
+const btnHitungLuas = document.querySelector(".btn-hitung-luas");
+const btnHitungKeliling = document.querySelector(".btn-hitung-keliling");
+const btnResetLuas = document.querySelector(".btn-reset-luas");
+const btnResetKeliling = document.querySelector(".btn-reset-keliling");
+const selectOption = document.querySelector(".list-menu");
+
+// Select dropdown value
+selectOption.addEventListener("change", function () {
+  const value = selectOption.value;
+  //   Check if value same as the option value
+  if (value === "keliling") {
+    document.querySelector(".keliling-persegi").style.display = "flex";
+    document.querySelector(".luas-persegi").style.display = "none";
+    //   Check if value same as the option value
+  } else if (value === "luas") {
+    document.querySelector(".luas-persegi").style.display = "flex";
+    document.querySelector(".keliling-persegi").style.display = "none";
+  }
+});
+
+// Handling Hitung Luas button
+btnHitungLuas.addEventListener("click", function () {
+  // Check if input is empty
+  if (inputNumberLuas.value === "") {
+    alert("Input angka dulu ya");
+  } else {
+    const sisiPersegi = Number(inputNumberLuas.value);
+
+    // Formula Luas
+    const result = sisiPersegi * sisiPersegi;
+
+    document.querySelector(".output_luas").style.visibility = "visible";
+    document.querySelector(".hasil-luas").textContent = `L = ${result}`;
+  }
+});
+
+// Handling Reset Button Luas Persegi
+btnResetLuas.addEventListener("click", function () {
+  inputNumberLuas.value = "";
+  document.querySelector(".output_luas").style.visibility = "hidden";
+});
