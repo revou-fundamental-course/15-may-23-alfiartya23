2b01d5c868a1ad47e6f5390261f360a2585122a0
alfiartya23 <alfiartya.diwanda@gmail.com>
2023-05-25 15:13:15 +0700

    Cleaning some codes in hmtl, js, css

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
 "use strict";
 
-// Take the html tag
+// Storing the HTML class and id inside the variable
 const inputNumberLuas = document.querySelector("#input-sisi-luas");
 const inputNumberKeliling = document.querySelector("#input-sisi-keliling");
+const formLuas = document.querySelector(".form-container-luas");
+const formKeliling = document.querySelector(".form-container-keliling");
 const btnHitungLuas = document.querySelector(".btn-hitung-luas");
 const btnHitungKeliling = document.querySelector(".btn-hitung-keliling");
 const btnResetLuas = document.querySelector(".btn-reset-luas");
 const btnResetKeliling = document.querySelector(".btn-reset-keliling");
 const selectOption = document.querySelector(".list-menu");
 
+// Regex pattern for only accept number
+const patternNumber = new RegExp(/^[0-9]+$/);
+
+// Function for reset button
+const clearUI = function (inputTag, formTag, outputClass, className) {
+  inputTag.value = "";
+  document.querySelector(outputClass).style.visibility = "hidden";
+  formTag.classList.remove(className);
+};
+
+// Function for show error
+const showError = function (formTag, errorClass, message) {
+  formTag.classList.add(errorClass);
+  const small = formTag.querySelector("small");
+  small.innerText = message;
+};
+
 // Select dropdown value
 selectOption.addEventListener("change", function () {
   const value = selectOption.value;
-  //   Check if value same as the option value
   if (value === "keliling") {
+    //   WHen user choose "keliling", check if value same as the option value
     document.querySelector(".keliling-persegi").style.display = "flex";
     document.querySelector(".luas-persegi").style.display = "none";
-    //   Check if value same as the option value
   } else if (value === "luas") {
+    //   WHen user choose "luas", check if value same as the option value
     document.querySelector(".luas-persegi").style.display = "flex";
     document.querySelector(".keliling-persegi").style.display = "none";
   }
@@ -25,31 +44,28 @@ selectOption.addEventListener("change", function () {
 
 // Handling Hitung Luas button
 btnHitungLuas.addEventListener("click", function (event) {
+  // Prevent button to submit
   event.preventDefault();
 
   // Check if input is empty
   if (inputNumberLuas.value === "") {
     // Validation if input is empty
-    const formContainer = document.querySelector(".form-container");
-    formContainer.classList.add("error");
-
-    const smallMessage = document.querySelector("small");
-    smallMessage.textContent = "Input tidak boleh kosong";
-  } else if (!/^[0-9]+$/.test(inputNumberLuas.value)) {
+    showError(formLuas, "error-luas", "Input tidak boleh kosong");
+  } else if (!patternNumber.test(inputNumberLuas.value)) {
     // Validation if input is not a number
-    const formContainer = document.querySelector(".form-container");
-    formContainer.classList.add("error");
+    showError(formLuas, "error-luas", "Hanya menerima angka!");
 
-    const smallMessage = document.querySelector("small");
-    smallMessage.innerText = "Hanya menerima angka!";
+    // Hide the output
     document.querySelector(".output_luas").style.visibility = "hidden";
   } else {
-    document.querySelector(".form-container").classList.remove("error");
-    const sisiPersegi = Number(inputNumberLuas.value);
+    // Remove error class when showing the result
+    formLuas.classList.remove("error-luas");
 
     // Formula Luas
+    const sisiPersegi = Number(inputNumberLuas.value);
     const result = sisiPersegi * sisiPersegi;
 
+    // Showing the result keliling persegi
     document.querySelector(".output_luas").style.visibility = "visible";
     document.querySelector(".hasil-luas").innerHTML = `Luas = ${result} cm<sup>2</sup>`;
   }
@@ -57,31 +73,28 @@ btnHitungLuas.addEventListener("click", function (event) {
 
 // Handling Hitung Keliling button
 btnHitungKeliling.addEventListener("click", function (event) {
+  // Prevent button to submit
   event.preventDefault();
 
   // Check if input is empty
   if (inputNumberKeliling.value === "") {
     // Validation if input is empty
-    const formContainer = document.querySelector(".form-container-keliling");
-    formContainer.classList.add("error-keliling");
-
-    const smallMessage = document.querySelector(".small-keliling");
-    smallMessage.textContent = "Input tidak boleh kosong";
-  } else if (!/^[0-9]+$/.test(inputNumberKeliling.value)) {
+    showError(formKeliling, "error-keliling", "Input tidak boleh kosong");
+  } else if (!patternNumber.test(inputNumberKeliling.value)) {
     // Validation if input is not a number
-    const formContainer = document.querySelector(".form-container-keliling");
-    formContainer.classList.add("error-keliling");
+    showError(formKeliling, "error-keliling", "Hanya menerima angka!");
 
-    const smallMessage = document.querySelector(".small-keliling");
-    smallMessage.innerText = "Hanya menerima angka!";
+    // Hide the output
     document.querySelector(".output_keliling").style.visibility = "hidden";
   } else {
-    document.querySelector(".form-container-keliling").classList.remove("error-keliling");
-    const sisiPersegi = Number(inputNumberKeliling.value);
+    // Remove error class when showing the result
+    formKeliling.classList.remove("error-keliling");
 
-    // Formula Luas
+    // Formula Keliling
+    const sisiPersegi = Number(inputNumberKeliling.value);
     const result = 4 * sisiPersegi;
 
+    // Showing the result keliling persegi
     document.querySelector(".output_keliling").style.visibility = "visible";
     document.querySelector(".hasil-keliling").textContent = `Keliling = ${result} cm`;
   }
@@ -90,15 +103,15 @@ btnHitungKeliling.addEventListener("click", function (event) {
 // Handling Reset Button Luas Persegi
 btnResetLuas.addEventListener("click", function (event) {
   event.preventDefault();
-  inputNumberLuas.value = "";
-  document.querySelector(".output_luas").style.visibility = "hidden";
-  document.querySelector(".form-container").classList.remove("error");
+
+  // Calling the clearUI function for reset button in Luas Persegi
+  clearUI(inputNumberLuas, formLuas, ".output_luas", "error-luas");
 });
 
 // Handling Reset Button Luas Persegi
 btnResetKeliling.addEventListener("click", function (event) {
   event.preventDefault();
-  inputNumberKeliling.value = "";
-  document.querySelector(".output_keliling").style.visibility = "hidden";
-  document.querySelector(".form-container-keliling").classList.remove("error-keliling");
+
+  // Calling the clearUI function for reset button in Keliling Persegi
+  clearUI(inputNumberKeliling, formKeliling, ".output_keliling", "error-keliling");
 });
